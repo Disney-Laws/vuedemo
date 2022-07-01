1c16496520f278036a45b7bdc10270dbfe71b8ad
fanmiao11 <miao623@outlook.com>
2022-07-01 21:26:50 +0800

    完成了品牌管理案例

--- fanmiao/vueday4/src/main.js
+++ fanmiao/vueday4/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 Vue.config.productionTip = false
 Vue.filter('reverse',(val,spl)=> val.split('').reverse().join(spl))
