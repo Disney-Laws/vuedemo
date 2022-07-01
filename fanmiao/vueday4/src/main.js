4c393cd26d941e74f6c89be7cc281a4ae680d10f
fanmiao11 <miao623@outlook.com>
2022-07-01 19:50:34 +0800

    练习过滤器的使用

--- fanmiao/vueday4/src/main.js
+++ fanmiao/vueday4/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.filter('reverse',(val,spl)=> val.split('').reverse().join(spl))
 
 new Vue({
   render: h => h(App),
