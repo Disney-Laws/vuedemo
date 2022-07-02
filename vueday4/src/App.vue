2c5eaa8ffcdca459ee3393496d8f363c5d7bf537
kotoriefreet <1063377232@qq.com>
2022-07-02 10:03:47 +0800

    反选

--- vueday4/src/App.vue
+++ vueday4/src/App.vue
@@ -2,12 +2,12 @@
   <div>
     <span>全选:</span>
     <!-- 4. v-model 关联全选 - 选中状态 -->
-    <input type="checkbox" v-model="isAll"/>
-    <button>反选</button>
+    <input type="checkbox" v-model="isAll" />
+    <button @click="btn">反选</button>
     <ul>
       <li v-for="(obj, index) in arr" :key="index">
         <!-- 3. 对象.c - 关联 选中状态 -->
-        <input type="checkbox" v-model="obj.c"/>
+        <input type="checkbox" v-model="obj.c" />
         <span>{{ obj.name }}</span>
       </li>
     </ul>
@@ -43,11 +43,23 @@ export default {
   },
   // 5. 计算属性-isAll
   computed: {
-    isAll () {
-         // 6. 统计小选框状态 ->  全选状态
+    isAll: {
+      set(val) {
+        // 7. 全选框 - 选中状态(true/false)
+        this.arr.forEach((obj) => (obj.c = val));
+      },
+      get() {
+        // 6. 统计小选框状态 ->  全选状态
         // every口诀: 查找数组里"不符合"条件, 直接原地返回false
-        return this.arr.every(obj => obj.c === true)
-    }
-  }
+        return this.arr.every((obj) => obj.c === true);
+      },
+    },
+  },
+  methods: {
+    btn() {
+      // 8. 让数组里对象的c属性取反再赋予回去
+      this.arr.forEach((obj) => (obj.c = !obj.c));
+    },
+  },
 };
 </script>
