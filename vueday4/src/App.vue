f82880aedea5daa9dafc4e8ced4057112a7d48ec
kotoriefreet <1063377232@qq.com>
2022-07-02 10:04:10 +0800

    侦听器

--- vueday4/src/App.vue
+++ vueday4/src/App.vue
@@ -1,65 +1,37 @@
 <template>
   <div>
-    <span>全选:</span>
-    <!-- 4. v-model 关联全选 - 选中状态 -->
-    <input type="checkbox" v-model="isAll" />
-    <button @click="btn">反选</button>
-    <ul>
-      <li v-for="(obj, index) in arr" :key="index">
-        <!-- 3. 对象.c - 关联 选中状态 -->
-        <input type="checkbox" v-model="obj.c" />
-        <span>{{ obj.name }}</span>
-      </li>
-    </ul>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
-// 目标: 小选框 -> 全选
-// 1. 标签+样式+js准备好
-// 2. 把数据循环展示到页面上
 export default {
-  data() {
+  data(){
     return {
-      arr: [
-        {
-          name: "猪八戒",
-          c: false,
-        },
-        {
-          name: "孙悟空",
-          c: false,
-        },
-        {
-          name: "唐僧",
-          c: false,
-        },
-        {
-          name: "白龙马",
-          c: false,
-        },
-      ],
-    };
+      name: ""
+    }
   },
-  // 5. 计算属性-isAll
-  computed: {
-    isAll: {
-      set(val) {
-        // 7. 全选框 - 选中状态(true/false)
-        this.arr.forEach((obj) => (obj.c = val));
-      },
-      get() {
-        // 6. 统计小选框状态 ->  全选状态
-        // every口诀: 查找数组里"不符合"条件, 直接原地返回false
-        return this.arr.every((obj) => obj.c === true);
-      },
-    },
-  },
-  methods: {
-    btn() {
-      // 8. 让数组里对象的c属性取反再赋予回去
-      this.arr.forEach((obj) => (obj.c = !obj.c));
-    },
-  },
-};
+  // 目标: 侦听到name值的改变
+  /*
+  语法:
+    watch: {
+      变量名 (newVal, oldVal){
+        // 变量名对应值改变这里自动触发
+        // newVal 是改变之后的值
+        // oldVal 是改变之前的值  
+      }
+    }
+  */
+  watch: {
+    // newVal: 当前最新值
+    // oldVal: 上一刻值
+    name(newVal, oldVal){
+      console.log(newVal, oldVal);
+    }
+  }
+}
 </script>
+
+<style>
+
+</style>
