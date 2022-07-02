919bb792f30a4091bb44f9b1202bb5297b5e67e1
kotoriefreet <1063377232@qq.com>
2022-07-02 10:04:44 +0800

    深度侦听

--- vueday4/src/App.vue
+++ vueday4/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <input type="text" v-model="name">
+    <input type="text" v-model="user.name">
+    <input type="text" v-model="user.age">
   </div>
 </template>
 
@@ -8,25 +9,36 @@
 export default {
   data(){
     return {
-      name: ""
+      user: {
+        name: "",
+        age: 0
+      }
     }
   },
-  // 目标: 侦听到name值的改变
+  // 目标: 侦听对象
   /*
   语法:
     watch: {
       变量名 (newVal, oldVal){
         // 变量名对应值改变这里自动触发
-        // newVal 是改变之后的值
-        // oldVal 是改变之前的值  
+      },
+      变量名: {
+        handler(newVal, oldVal){
+
+        },
+        deep: true, // 深度侦听(对象里面层的值改变)
+        immediate: true // 立即侦听(网页打开handler执行一次)
       }
     }
   */
   watch: {
-    // newVal: 当前最新值
-    // oldVal: 上一刻值
-    name(newVal, oldVal){
-      console.log(newVal, oldVal);
+    user: {
+      handler(newVal, oldVal){
+        // user里的对象
+        console.log(newVal, oldVal);
+      },
+      deep: true,
+      immediate: true
     }
   }
 }
