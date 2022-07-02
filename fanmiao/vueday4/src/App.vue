df7be892872ec3df605c386e9650ada0b71bd590
fanmiao11 <miao623@outlook.com>
2022-07-02 17:06:18 +0800

    完成this指向

--- fanmiao/vueday4/src/App.vue
+++ fanmiao/vueday4/src/App.vue
@@ -6,23 +6,27 @@
 
 <script >
 
-// <!-- 2. 模拟实现instanceof -->
-function instance_of(L, R) { // L 表示左表达式，R 表示右表达式
-  const O = R.prototype;
-  L = L.__proto__;
-  while (true) {
-    if (L === null) { // Object.prototype 为 null
-      return false
-    }
-    if (O === L) { // 当 O 严格等于 L 时，返回 true 
-      return true
-    }
-    L = L.__proto__
-  }
+
+// <!-- 3. this 指向 -->
+function fn() {
+  console.log(this);
 }
-console.log(instance_of(Object, Object));
-console.log(instance_of(Object, Function));
-console.log(instance_of(String, String));
+
+const obj = {
+  name: "test",
+  a() {
+    console.log(this.a);
+    setTimeout(function () {
+      console.log(this.a);
+    }, 1000);
+  },
+  b() {
+    console.log(this);
+    fn();
+  },
+};
+obj.a();
+obj.b();
 
 
 export default {
