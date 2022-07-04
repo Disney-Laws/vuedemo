a4c1ab28f633a3456958e3ed54fd16607c17c712
huchun <1575724298@qq.com>
2022-07-04 22:19:56 +0800

    init project

---
+++ hc/day06bookslist/src/App.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: suiyue
+ * @email: 1373842098@qq.com
+ * @Date: 2022-07-04 22:17:41
+ * @LastEditors: sj
+ * @LastEditTime: 2022-07-04 22:19:08
+-->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
