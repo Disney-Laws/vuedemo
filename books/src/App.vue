5aba0491aabd504388e1e0742d47df41d8b38589
kotoriefreet <1063377232@qq.com>
2022-07-05 18:42:19 +0800

    获取图书

--- books/src/App.vue
+++ books/src/App.vue
@@ -1,11 +1,45 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <GetAdd :bookList="bookList"></GetAdd>
+    <PostAdd :bookList="bookList" @delBook="delBookFn"></PostAdd>
+    <DelAdd></DelAdd>
+  </div>
 </template>
 
 <script>
+import GetAdd from "./components/GetAdd.vue";
+import PostAdd from "./components/PostAdd.vue";
+import DelAdd from "./components/DelAdd.vue";
+
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      bookList: [],
+    };
+  },
+  components: {
+    GetAdd,
+    PostAdd,
+    DelAdd,
+  },
+  created() {
+    this.getBookList();
+  },
+  methods: {
+    delBookFn(id) {
+      const index = this.bookList.findIndex((ele) => ele.id == id);
+      this.bookList.splice(index, 1);
+    },
+    getBookList() {
+      this.$axios({
+        url: "/api/getbooks",
+      }).then((res) => {
+        console.log(res);
+        this.bookList = res.data.data;
+      });
+    },
+  },
 };
 </script>
 
