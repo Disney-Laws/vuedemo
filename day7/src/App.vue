54f393d0a5b6d7c0b28468ecd4ffe78b87d1de9b
kotoriefreet <1063377232@qq.com>
2022-07-06 20:52:55 +0800

    初始

---
+++ day7/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <MyTest></MyTest>
+  </div>
+</template>
+
+<script>
+import MyTest from "./views/UseTable.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    MyTest,
+  },
+};
+</script>
+
+<style></style>
---
+++ day7/src/components/MyTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>头像</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(obj, index) in arr" :key="index">
+          <td>
+            <slot name="index" :row="obj">{{ index + 1 }}</slot>
+          </td>
+          <td>
+            <slot name="name" :row="obj">{{ obj.name }}</slot>
+          </td>
+          <td>
+            <slot name="age" :row="obj">{{ obj.age }}</slot>
+          </td>
+          <td>
+            <slot name="img" :row="obj">{{ obj.headImgUrl }} </slot>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    arr: Array,
+  },
+};
+</script>
