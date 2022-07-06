92975a6e5b529750d7e45006413340a3a0d7b909
fanmiao11 <miao623@outlook.com>
2022-07-06 20:36:11 +0800

    练习案例3 作用域插槽使用场景

--- fanmiao/vueday7/src/App.vue
+++ fanmiao/vueday7/src/App.vue
@@ -3,17 +3,22 @@
     <!-- 案例1 动态组件+组件缓存 -->
     <!-- <Dynamic></Dynamic> -->
     <!-- 案例2  组件插槽-->
-    <Slot></Slot>
+    <!-- <Slot></Slot> -->
+    <!-- 案例3 作用域插槽使用场景 -->
+    <MyTable></MyTable>
+
   </div>
 </template>
 
 <script>
 import Dynamic from './CaseTest/01_UseDynamic.vue'
 import Slot from './CaseTest/02_UseSlot.vue'
+import MyTable from './CaseTest/03_UseTable.vue'
 export default {
   components:{
     Dynamic,
     Slot,
+    MyTable,
   }
 
 }
---
+++ fanmiao/vueday7/src/components/CaseTest/03/MyTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <table border="1">
+          <thead>
+              <tr>
+                  <th>序号</th>
+                  <th>姓名</th>
+                  <th>年龄</th>
+                  <th>头像</th>
+              </tr>
+          </thead>
+          <thead>
+              <tr v-for="(item,index) in arr" :key="index">
+                  <td>{{index}}</td>
+                  <td>{{item.name}}</td>
+                  <td>
+                    <slot name="age" :row="item"></slot>
+                  </td>
+                  <td>
+                    <slot name="Img" :row="item"></slot>
+                  </td>
+              </tr>
+          </thead>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        
+    }
+},
+props:['arr'],
+
+}
+</script>
