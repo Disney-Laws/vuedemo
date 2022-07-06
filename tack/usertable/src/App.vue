77e31e0acdf5ace19932efb9678f91e254f52f5f
haiou1314 <1871943511@qq.com>
2022-07-06 23:15:44 +0800

    usertable案例提交

---
+++ tack/usertable/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <usertatleVue></usertatleVue>
+  </div>
+</template>
+
+<script>
+
+import usertatleVue from './components/usertatle.vue'
+export default {
+  name: 'App',
+  components: {
+   usertatleVue,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tack/usertable/src/components/usertatle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <MyTableVue>
+     <template #imgage="scoped">
+        <img :src="scoped.img" alt="" style="width:80px">
+     </template>
+       <template #age="scoped">
+        <input type="text" :value="scoped.ip">
+     </template>
+       <template #name="scoped">
+       <p>{{scoped.nm}}</p>
+     </template>
+       <template #index="scoped">
+       <span>{{scoped.ind}}</span>
+     </template>
+    </MyTableVue>
+  </div>
+</template>
+
+<script>
+import MyTableVue from '../view/MyTable.vue'
+export default {
+  components:{
+    MyTableVue
+  }
+}
+</script>
