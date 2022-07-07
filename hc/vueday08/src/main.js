87b2387e52778b62dc9646f59fe63c4f4eade5f2
huchun <1575724298@qq.com>
2022-07-07 20:25:30 +0800

    完成列表数据渲染

--- hc/vueday08/src/main.js
+++ hc/vueday08/src/main.js
@@ -5,12 +5,16 @@
  * @email: 1373842098@qq.com
  * @Date: 2022-07-07 19:22:46
  * @LastEditors: sj
- * @LastEditTime: 2022-07-07 19:27:37
+ * @LastEditTime: 2022-07-07 20:05:22
  */
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+axios.defaults.baseURL= "https://www.escook.cn"
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
--- hc/vueday08/src/views/MyGoodsList.vue
+++ hc/vueday08/src/views/MyGoodsList.vue
@@ -5,17 +5,63 @@
  * @email: 1373842098@qq.com
  * @Date: 2022-07-07 19:28:56
  * @LastEditors: sj
- * @LastEditTime: 2022-07-07 19:49:17
+ * @LastEditTime: 2022-07-07 20:24:24
 -->
 <template>
   <div>
-    MyGoodsList
+    <MyTable :list="list">
+      <template #header>
+          <th>#</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th>
+      </template>
+      <template #body="scope">
+        <td>{{scope.row.id}}</td>
+        <td>{{scope.row.goods_name}}</td>
+        <td>{{scope.row.goods_price}}</td>
+        <td>
+             <span
+            class="badge badge-warning"
+            v-for="(item, index) in scope.row.tags"
+            :key="index"
+            style="margin-left: 10px"
+            >{{ item }}</span
+          >
+        </td>
+        <td>
+          <button class="btn btn-danger btn-sm">删除</button>
+          <button class="btn btn-primary btn-sm">编辑</button>
+        </td>
+      </template>
+    </MyTable>
   </div>
 </template>
 
 <script>
+import MyTable from '../components/MyTable.vue'
 export default {
-
+  components:{
+    MyTable
+  },
+  data(){
+    return {
+      list:[]
+    }
+  },
+  created(){
+    this.getList()
+  },
+  methods:{
+    getList(){
+      this.$axios({
+        url:"/api/goods"
+      }).then(res=>{
+        console.log(res.data.data);
+        this.list = res.data.data})
+    }
+  }
 }
 </script>
 
