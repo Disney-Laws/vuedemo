09ac8fe057897b2f4437f31b2a3c1f069d3f4f37
fanmiao11 <miao623@outlook.com>
2022-07-07 19:43:38 +0800

    初始化tabbar 创建组件

--- fanmiao/vueday8/src/App.vue
+++ fanmiao/vueday8/src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
     <!-- 自定义指令 -->
-  <UseDirective></UseDirective>
+  <!-- <UseDirective></UseDirective> -->
+  <!-- tabbar 案例 -->
+  <MyHeader title="tabbar"></MyHeader>
+  <MyTable></MyTable>
+  <MyTabBar :arr="tabList"></MyTabBar>
+
   </div>
 </template>
 
 <script>
 
 import UseDirective from './components/UseDirective.vue'
-export default {
+import MyHeader from './components/tabbar/MyHeader.vue'
+import MyTabBar from './components/tabbar/MyTabBar.vue'
+import MyTable from './components/tabbar/MyTable.vue'
 
+export default {
+  data(){
+    return{
+
+      tabList: [
+      {
+          iconText: "icon-shangpinliebiao",
+          text: "商品列表",
+          componentName: "MyGoodsList"
+      },
+      {
+          iconText: "icon-sousuo",
+          text: "商品搜索",
+          componentName: "MyGoodsSearch"
+      },
+      {
+          iconText: "icon-user",
+          text: "我的信息",
+          componentName: "MyUserInfo"
+      }
+  ]
+    }
+  },
   components: {
     UseDirective,
+    MyHeader,
+    MyTabBar,
+    MyTable,
 
   }
 }
--- fanmiao/vueday8/src/main.js
+++ fanmiao/vueday8/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+ import 'bootstrap/dist/css/bootstrap.css'
+ import './assets/fonts/iconfont.css'
+
 // 全局指令
 Vue.directive('gfocus',{
   inserted(el){
