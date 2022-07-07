e5c60c81b6acd50a9b7992c1955ac357336d2851
huchun <1575724298@qq.com>
2022-07-07 19:48:43 +0800

    完成了头部底部

--- hc/vueday08/src/App.vue
+++ hc/vueday08/src/App.vue
@@ -5,17 +5,44 @@
  * @email: 1373842098@qq.com
  * @Date: 2022-07-07 19:22:46
  * @LastEditors: sj
- * @LastEditTime: 2022-07-07 19:23:52
+ * @LastEditTime: 2022-07-07 19:35:09
 -->
 <template>
   <div>
-    
+<MyHeader title="TabBar"></MyHeader>
+<MyTabBar :arr="tabList"></MyTabBar>
   </div>
 </template>
 
 <script>
+import MyHeader from './components/MyHeader.vue'
+import MyTabBar from './components/MyTabBar.vue'
 export default {
-
+ data(){
+  return {
+    tabList: [
+    {
+        iconText: "icon-shangpinliebiao",
+        text: "商品列表",
+        componentName: "MyGoodsList"
+    },
+    {
+        iconText: "icon-sousuo",
+        text: "商品搜索",
+        componentName: "MyGoodsSearch"
+    },
+    {
+        iconText: "icon-user",
+        text: "我的信息",
+        componentName: "MyUserInfo"
+    }
+]
+  }
+ },
+ components:{
+  MyHeader,
+  MyTabBar
+ }
 }
 </script>
 
--- hc/vueday08/src/components/MyTabBar.vue
+++ hc/vueday08/src/components/MyTabBar.vue
@@ -5,5 +5,70 @@
  * @email: 1373842098@qq.com
  * @Date: 2022-07-07 19:28:12
  * @LastEditors: sj
- * @LastEditTime: 2022-07-07 19:28:12
+ * @LastEditTime: 2022-07-07 19:45:39
 -->
+<template>
+  <div class="my-tab-bar">
+  	<div 
+    class="tab-item"
+    v-for="(item, index) in arr" :key="index"
+    :class="{current: currentIndex==index}"
+    @click="currentIndex=index"
+    >
+      <!-- 图标 -->
+      <span 
+      class="iconfont"
+      :class="item.iconText"
+      ></span>
+      <!-- 文字 -->
+      <span>{{item.text}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      currentIndex:0
+    }
+  },
+  props:{
+    arr:{
+      type:Array,
+      validator(val){
+           // val  接收到到的list
+        if(val.length>=2&&val.length<=5){
+          return true
+        }else{
+          return false
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.my-tab-bar {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  border-top: 1px solid #ccc;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+  .tab-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+    
+.current {
+  color: #1d7bff;
+}
+</style>
