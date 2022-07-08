a2c343fa26472ae529866b4c17e8b6f00c1ab4ba
kotoriefreet <1063377232@qq.com>
2022-07-08 10:14:13 +0800

    列表

--- tabbar-demo/src/components/MyTable.vue
+++ tabbar-demo/src/components/MyTable.vue
@@ -1,35 +1,53 @@
 <template>
-  <div class="my-tab-bar">
-    <div
-      class="tab-item"
-      v-for="(obj, index) in arr"
-      :key="index"
-    >
-      <!-- 图标 -->
-      <span class="iconfont" :class="obj.iconText"></span>
-      <!-- 文字 -->
-      <span>{{ obj.text }}</span>
-    </div>
-  </div>
+  <table class="table table-bordered table-stripped">
+    <!-- 表格标题区域 -->
+    <thead>
+      <tr>
+        <!-- <th>#</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th> -->
+        <slot name="header"></slot>
+      </tr>
+    </thead>
+    <!-- 表格主体区域 -->
+    <tbody>
+      <tr v-for="item in list" :key="item.componentName">
+        <!-- <td>{{ item.id }}</td>
+        <td>{{ item.goods_name }}</td>
+        <td>{{ item.goods_price }}</td>
+        <td>{{ item.tags }}</td>
+        <td>
+            <button class="btn btn-danger btn-sm">删除</button>
+        </td> -->
+        <slot name="body" :row="item"></slot>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 export default {
+  name: "MyTable",
   props: {
-    arr: {
+    list: {
       type: Array,
-      required: true,
-      // 自定义校验规则
-      validator(value) {
-        // value就是接到数组
-        if (value.length >= 2 && value.length <= 5) {
-          return true; // 符合条件就return true
-        } else {
-          console.error("数据源必须在2-5项");
-          return false;
-        }
-      },
+      // validator(val) {
+      //   if (val.length >= 2 && val.length <= 5) {
+      //     return true;
+      //   }
+      //   return false;
+      // },
     },
-  }
+  },
 };
 </script>
+
+<style scoped lang="less">
+.my-goods-list {
+  .badge {
+    margin-right: 5px;
+  }
+}
+</style>
--- tabbar-demo/src/main.js
+++ tabbar-demo/src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/fonts/iconfont.css";
+
+import axios from "axios";
+axios.defaults.baseURL = "http://www.escook.cn";
+Vue.prototype.$axios = axios;
+
 Vue.config.productionTip = false;
 
 new Vue({
