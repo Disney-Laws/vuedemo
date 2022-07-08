5c2016517e554e37191640bbb56e5583fdd5f86b
kotoriefreet <1063377232@qq.com>
2022-07-08 10:16:15 +0800

    头部

--- tabbar-demo/src/App.vue
+++ tabbar-demo/src/App.vue
@@ -8,7 +8,9 @@
     <div class="main">
       <component :is="comName"></component>
     </div>
-    <MyTabBar :arr="tabList" @changeCom="changeComFn"></MyTabBar>
+    <MyTabBar :list="tabList"
+    @changeCom="changeComFn"
+    ></MyTabBar>
   </div>
 </template>
 
@@ -21,7 +23,7 @@ import MyHeader from "./components/MyHeader";
 // 4. 使用MyTabBar组件, 传入数据(父->子), 循环产生底部导航
 // 5. 子组件内props自定义检验规则(2-5项)
 // 6. 子组件内循环产生底部导航
-import MyTabBar from "./components/MyTabBar";
+import MyTabBar from './components/MyTabBar'
 
 // 目标: 切换组件显示
 // 1. 创建组件 - 编写内容
@@ -31,15 +33,14 @@ import MyTabBar from "./components/MyTabBar";
 // 5. 底部导航点击- MyTabBar.vue里
 // 子 -> 父技术 (传要切换的组件名出来)
 
-import MyGoodsList from "./views/MyGoodsList";
-import MyGoodsSearch from "./views/MyGoodsSearch";
-import MyUserInfo from "./views/MyUserInfo";
+import MyGoodsList from './views/MyGoodsList'
+import MyGoodsSearch from './views/MyGoodsSearch'
+import MyUserInfo from './views/MyUserInfo'
 export default {
   data() {
     return {
       comName: "MyGoodsList", // 默认显示的组件
-      tabList: [
-        // 底部导航的数据
+      tabList: [ // 底部导航的数据
         {
           iconText: "icon-shangpinliebiao",
           text: "商品列表",
@@ -63,19 +64,20 @@ export default {
     MyTabBar,
     MyGoodsList,
     MyGoodsSearch,
-    MyUserInfo,
+    MyUserInfo
   },
   methods: {
-    changeComFn(cName) {
+    changeComFn(cName){
+      
       this.comName = cName; // MyTabBar里选出来的组件名赋予给is属性的comName
       // 导致组件的切换
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
-.main {
+.main{
   padding-top: 45px;
   padding-bottom: 51px;
 }
--- tabbar-demo/src/views/MyGoodsList.vue
+++ tabbar-demo/src/views/MyGoodsList.vue
@@ -1,14 +1,57 @@
 <template>
   <div>
-    <MyTable :arr="list">
+    <MyTable :list="list">
+      <template #header>
+        <th>#</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th>
+      </template>
+      <!-- scope的值: {row: obj} -->
+      <template #body="scope">
+        <td>{{ scope.row.id }}</td>
+        <td>{{ scope.row.goods_name }}</td>
+        <td>{{ scope.row.goods_price }}</td>
+        <td>
+          <input
+          class="tag-input form-control"
+          style="width: 100px;"
+          type="text"
+          v-if="scope.row.inputVisible"
+          v-focus
+          @blur="scope.row.inputVisible = false"
+          @keydown.enter="enterFn(scope.row)"
+          v-model="scope.row.inputValue"
+          @keydown.esc="scope.row.inputValue = ''"
+          />
+          <button 
+          v-else 
+          style="display: block;" 
+          class="btn btn-primary btn-sm add-tag"
+          @click="scope.row.inputVisible = true"
+          >+Tag</button>
+
+          <span v-for="(str, ind) in scope.row.tags" :key="ind"
+          class="badge badge-warning"
+          >
+            {{ str }}
+          </span>
+        </td>
+        <td>
+          <button class="btn btn-danger btn-sm"
+          @click="removeBtn(scope.row.id)"
+          >删除</button>
+        </td>
+      </template>
     </MyTable>
   </div>
 </template>
 
 <script>
 import MyTable from "../components/MyTable";
-import axios from "axios";
-axios.defaults.baseURL = "https://www.escook.cn";
+// import axios from "axios";
+// axios.defaults.baseURL = "https://www.escook.cn";
 // 目标: 循环商品列表表格
 // 1. 封装MyTable.vue 整体表格组件-一套标签和样式
 // 2. axios请求数据
@@ -20,10 +63,8 @@ axios.defaults.baseURL = "https://www.escook.cn";
 
 // 目标: 删除数据
 // 1. 删除按钮 - 点击事件
-// 2. 作用域插槽把索引值关联出来了
-// scope身上就有row和index
-// 3. 删除中使用scope.index的索引值
-// 4. 删除事件里删除数组里对应索引值的数据
+// 2. 通过作用域插槽拿到id, 实现点击事件方法
+// 3. 通过id查找数组里数据, 找到索引
 export default {
   components: {
     MyTable,
@@ -34,12 +75,31 @@ export default {
     };
   },
   created() {
-    axios({
+    $axios({
       url: "/api/goods",
     }).then((res) => {
       console.log(res);
       this.list = res.data.data;
     });
+  },
+  methods: {
+    removeBtn(id){
+      let index = this.list.findIndex(obj => obj.id === id)
+      this.list.splice(index, 1)
+    },
+    enterFn(obj){ // 回车
+      // console.log(obj.inputValue);
+      if (obj.inputValue.trim().length === 0) {
+        alert('请输入数据')
+        return
+      }
+
+      obj.tags.push(obj.inputValue) // 表单里的字符.串状态t ags数组
+      obj.inputValue = ""
+    }
   }
 };
 </script>
+
+<style>
+</style>
