654d0e50ae292b20df2f85d8714ec5e51a4d112f
fanmiao11 <miao623@outlook.com>
2022-07-09 19:42:02 +0800

    练习三级路由嵌套 封装调整文件结构

---
+++ fanmiao/vueday9/src/1.js
@@ -0,0 +1,91 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+import Find from '@/views/Find.vue';
+import My from '@/views/My.vue';
+import Part from '@/views/Part.vue';
+// 引入404页面
+import NotFound from '@/views/NotFound.vue';
+// 引入二级嵌套页面
+import Ranking from '@/views/Second/Ranking.vue';
+import Recommend from '@/views/Second/Recommend.vue';
+import SongList from '@/views/Second/SongList.vue';
+// 引入三级嵌套路由
+import A from '@/views/Second/Third/a.vue'
+import B from '@/views/Second/Third/b.vue'
+import C from '@/views/Second/Third/c.vue'
+
+// 1、下载路由
+// 2、引入路由
+import VueRouter from 'vue-router';
+// 3、使用
+Vue.use(VueRouter);
+// 4、创建路由规则
+const routes = [
+  {
+    path: '/find',
+    name: 'Find', //给路由起名字
+    component: Find,
+    children: [
+      {
+        path: 'ranking',
+        component: Ranking,
+        children:[
+            {
+                path:'a',
+                component:A
+            },
+            {
+                path:'b',
+                component:B
+            },
+            {
+                path:'c',
+                component:C
+            },
+        ]
+      },
+      {
+        path: 'recommend',
+        component: Recommend,
+      },
+      {
+        path: 'songlist',
+        component: SongList,
+      },
+    ],
+  },
+  {
+    path: '/my',
+    name: 'My',
+    component: My,
+  },
+  {
+    path: '/part',
+    name: 'Part',
+    component: Part,
+  },
+  // 导航重定向
+  {
+    path: '/',
+    redirect: '/find',
+  },
+  {
+    path: '*',
+    component: NotFound,
+  },
+];
+// 5、创建路由
+const router = new VueRouter({
+  routes,
+  // 路由在地址栏的模式
+  // mode:'history',
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+  // 6、关联到vue实例
+  router,
+}).$mount('#app');
--- fanmiao/vueday9/src/main.js
+++ fanmiao/vueday9/src/main.js
@@ -1,74 +1,13 @@
-import Vue from 'vue';
-import App from './App.vue';
+import '@/1.js';
 
-import Find from '@/views/Find.vue'
-import My from '@/views/My.vue';
-import Part from '@/views/Part.vue';
-// 引入404页面
-import NotFound from '@/views/NotFound.vue'
-// 引入二级嵌套页面
-import Ranking from '@/views/Second/Ranking.vue'
-import Recommend from '@/views/Second/Recommend.vue';
-import SongList from '@/views/Second/SongList.vue';
+// import Vue from 'vue';
+// import App from './App.vue';
 
 
-// 1、下载路由
-// 2、引入路由
-import VueRouter from 'vue-router';
-// 3、使用
-Vue.use(VueRouter);
-// 4、创建路由规则
-const routes = [
-  {
-    path: '/find',
-    name: 'Find', //给路由起名字
-    component: Find,
-    children: [
-      {
-        path: 'ranking',
-        component: Ranking,
-      },
-      {
-        path: 'recommend',
-        component: Recommend,
-      },
-      {
-        path: 'songlist',
-        component: SongList,
-      },
-    ],
-  },
-  {
-    path: '/my',
-    name: 'My',
-    component: My,
-  },
-  {
-    path: '/part',
-    name: 'Part',
-    component: Part,
-  },
-  // 导航重定向
-  {
-    path: '/',
-    redirect: '/find',
-  },
-  {
-    path: '*',
-    component: NotFound,
-  },
-];
-// 5、创建路由
-const router = new VueRouter({
-  routes,
-  // 路由在地址栏的模式
-  // mode:'history',
-})
+// Vue.config.productionTip = false;
 
-Vue.config.productionTip = false;
-
-new Vue({
-  render: (h) => h(App),
-  // 6、关联到vue实例
-  router,
-}).$mount('#app');
+// new Vue({
+//   render: (h) => h(App),
+//   // 6、关联到vue实例
+//   // router,
+// }).$mount('#app');
