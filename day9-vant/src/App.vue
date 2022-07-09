8f69367a1ab38bd7efb1979567acab88cf24069c
kotoriefreet <1063377232@qq.com>
2022-07-09 18:50:27 +0800

    按钮

--- day9-vant/src/App.vue
+++ day9-vant/src/App.vue
@@ -1,19 +1,33 @@
 <template>
-  <div id="app">
-
+  <div>
+    <van-button type="primary" @click="btn">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
+// 方式2: 手动 按需引入
+// import Button from 'vant/lib/button'; // button组件
+// import 'vant/lib/button/style'; // button样式
 
+// 目标: 使用弹出框
+// 1. 找到vant文档
+// 2. 引入
+// 3. 在恰当时机, 调用此函数 (还可以用组件的用法)
+import { Dialog } from "vant";
 export default {
-  name: 'App',
-  components: {
-  
-  }
-}
+  // components: { // 手动注册组件名
+  //   // VanButton: Button
+  //   // 等价的
+  //   [Button.name]: Button
+  // }
+  methods: {
+    btn() {
+      Dialog({ message: "提示", showCancelButton: true }); // 调用执行时, 页面就会出弹出框
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
