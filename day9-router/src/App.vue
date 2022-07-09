acf2b3215e71a93e45167aee8f70acccaef9be06
kotoriefreet <1063377232@qq.com>
2022-07-09 19:00:57 +0800

    router

--- day9-router/src/App.vue
+++ day9-router/src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="footer_wrap">
+      <!-- <a href="#/find">发现音乐</a>
+      <a href="#/my">       我的音乐</a>
+      <a href="#/part">     朋友    </a> -->
+      <!-- <router-link to="/find">发现音乐</router-link>
+      <router-link to="/my">我的音乐</router-link>
+      <router-link to="/part/test/11">朋友</router-link> -->
+      <span @click="btn('/find', 'Find')">发现音乐</span>
+      <span @click="btn('/my', 'My')">我的音乐</span>
+      <span @click="btn('/part', 'Part')">朋友</span>
+    </div>
+    <div class="top">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// 目标: 声明式导航 - 激活类名区别
+// 1. url上hash值(#/home/recommend) 包含 导航的href值(#/home) - 当前a就有 "router-link-active"  (模糊)
+// 2. url上hash值(#/home/recommend) 等于 导航的href值(#/home/recommend) - 当前a就有"router-link-exact-active" (精确)
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  methods: {
+    btn(targetPath, targetName) {
+      this.$router.push({
+        // path: targetPath,
+        name: targetName,
+        query: {
+          id: 1,
+        },
+      });
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.footer_wrap {
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #333;
+  color: #ccc;
+}
+.footer_wrap span {
+  flex: 1;
+  text-decoration: none;
+  padding: 20px 0;
+  line-height: 20px;
+  background-color: #333;
+  color: #ccc;
+  border: 1px solid black;
+}
+.footer_wrap span:hover {
+  background-color: #555;
+}
+.top {
+  padding-top: 62px;
+}
+
+/*激活时样式 */
+.footer_wrap .router-link-active {
+  color: white;
+  background: black;
 }
 </style>
--- day9-router/src/main.js
+++ day9-router/src/main.js
@@ -1,8 +1,58 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Find from "@/views/Find"; // @是src的绝对地址
+import My from "@/views/My";
+import Part from "@/views/Part";
+import NotFound from "@/views/NotFound";
+import Recommend from "@/views/Second/Recommend";
+import Ranking from "@/views/Second/Ranking";
+import SongList from "@/views/Second/SongList";
+import VueRouter from "vue-router";
 
-Vue.config.productionTip = false
+let routerPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return routerPush.call(this, location).catch((err) => err);
+};
+
+// 3. 注册全局组件
+Vue.use(VueRouter);
+
+// 4. 规则数组
+const routes = [
+  { path: "/", redirect: "/find" },
+  {
+    path: "/find",
+    component: Find,
+    name: "Find",
+    children: [
+      { path: "ranking", component: Ranking },
+      { path: "recommend", component: Recommend },
+      { path: "songList", component: SongList },
+    ],
+  },
+  { path: "/my", component: My, name: "My" },
+  { path: "/part", component: Part, name: "Part" },
+  { path: "*", component: NotFound },
+];
+// 5. 生成路由对象
+const router = new VueRouter({
+  routes, // routes是固定key(传入规则数组)
+  mode: "history", // 默认不写是"hash"
+});
+
+Vue.config.productionTip = false;
+
+const islogin = true;
+router.beforeEach((to, from, next) => {
+  if (to.path !== "/part") {
+    next("part");
+  } else {
+    next();
+  }
+});
+// 6. 路由对象注入到vue实例中, this可以访问$route和$router
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+}).$mount("#app");
