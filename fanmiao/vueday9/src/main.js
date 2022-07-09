679e2b7038ae070fcc951c93eb5db2ddfa56097d
fanmiao11 <miao623@outlook.com>
2022-07-09 18:03:49 +0800

    练习vue路由基础使用

--- fanmiao/vueday9/src/main.js
+++ fanmiao/vueday9/src/main.js
@@ -1,8 +1,39 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import Find from '@/views/Find.vue'
+import My from '@/views/My.vue';
+import Part from '@/views/Part.vue';
+
+// 1、下载路由
+// 2、引入路由
+import VueRouter from 'vue-router';
+// 3、使用
+Vue.use(VueRouter);
+// 4、创建路由规则
+const routes = [
+  {
+    path: '/find',
+    component: Find,
+  },
+  {
+    path: '/my',
+    component: My,
+  },
+  {
+    path: '/part',
+    component: Part,
+  },
+];
+// 5、创建路由
+const router = new VueRouter({
+  routes,
+})
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  // 6、关联到vue实例
+  router,
+}).$mount('#app');
