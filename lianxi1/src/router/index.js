fc847fd61a2310eeac1c3dda19b383382c2b90b5
kotoriefreet <1063377232@qq.com>
2022-07-09 19:58:44 +0800

    附加练习一

--- lianxi1/src/router/index.js
+++ lianxi1/src/router/index.js
@@ -1,11 +1,9 @@
 import Vue from "vue";
-import App from "./App.vue";
-
+import VueRouter from "vue-router";
 import Home from "@/views/Home";
 import Cate from "@/views/Cate";
 import Order from "@/views/Order";
 import My from "@/views/My";
-import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
@@ -17,32 +15,23 @@ const routes = [
   {
     path: "/home",
     component: Home,
-    name: "home",
   },
   {
     path: "/cate",
     component: Cate,
-    name: "Cate",
   },
   {
     path: "/order",
     component: Order,
-    name: "Order",
   },
   {
     path: "/my",
     component: My,
-    name: "My",
   },
 ];
 
 const router = new VueRouter({
-  routes, // routes是固定key(传入规则数组)
+  routes,
 });
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
+export default router;
