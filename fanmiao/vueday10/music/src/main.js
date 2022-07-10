df98fc04c1c276804f179bdb561197e1105ba384
fanmiao11 <miao623@outlook.com>
2022-07-10 20:18:47 +0800

    vant一次性导入所有组件

--- fanmiao/vueday10/music/src/main.js
+++ fanmiao/vueday10/music/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 // 引入路由对象
 import router from '@/router'
 
+// vant 一次性导入所有组件
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+
 Vue.config.productionTip = false
 
 new Vue({
